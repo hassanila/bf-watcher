5e1150b5894e6a762a152d302fdc41385dc0eb0e
Hassan <hassanila97@users.noreply.github.com>
2019-06-16 00:30:56 +0200

    First Release

---
+++ bak/bf-watcher-2018-11-10.sql
@@ -0,0 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+01:00";
+
+
+--
+-- Database: `bf`
+--
+
+DROP DATABASE IF EXISTS `bf`;
+CREATE DATABASE `bf`;
+USE `bf`;
+
+
+CREATE TABLE `ava***REMOVED***ble` (
+  `id` varchar(50) DEFAULT NULL
+);
+
+
+--
+-- Indexes for table `ava***REMOVED***ble`
+--
+ALTER TABLE `ava***REMOVED***ble`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
